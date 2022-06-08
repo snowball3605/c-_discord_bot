23b573ff4aca1a7b3ad809e0fa9ff1343ee4b4d6
snowball_213 <81451339+weslychan1212@users.noreply.github.com>
2022-06-08 22:01:13 +0800

    Add files via upload

---
+++ bot/bot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Webhook;
+using Discord.WebSocket;
+
+namespace bot
+{
+
+    class Bot
+    {
+        static void Main()
+        {
+            
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordC)
+        }
+
+    }
+}
